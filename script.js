ddd9708db2dd76bffc2a48ab2a4b68b99235fcb8
helovbm <161361957+helovbm@users.noreply.github.com>
2026-09-03 08:36:09 -0300

    .

---
+++ script.js
@@ -0,0 +1,6 @@
+// Seleciona um botão na página e adiciona um evento de clique
+const botao = document.querySelector('.meu-botao');
+
+botao.addEventListener('click', () => {
+  alert('Você clicou no botão!');
+});
